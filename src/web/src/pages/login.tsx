f1e008ae16631ac321885849bcb083c07948fdef
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-06-29 11:22:08 +0100

    one last attempt otherwise will try non /.auth/login/google approach

--- src/web/src/pages/login.tsx
+++ src/web/src/pages/login.tsx
@@ -11,13 +11,12 @@ const Login = () => {
               <BaseButton href="/.auth/login/google">Login with Google</BaseButton>
             </Stack>
             <Stack>
-              trackEvent(ActionTypes.LOGIN_PAGE.toString());
-              <BaseButton href={`/.auth/login/google?post_login_redirect_uri=${encodeURIComponent('/login-redirect')}`}>
+              <BaseButton href={`/.auth/login/google?post_login_redirect_uri=/login-redirect`}>
                 Login with Google redirect
               </BaseButton>
             </Stack>
             <Stack>
-              <BaseButton href={`/.auth/login/aad?post_login_redirect_uri=${encodeURIComponent('/login-redirect')}`}>
+              <BaseButton href={`/.auth/login/aad?post_login_redirect_uri=/login-redirect`}>
                 console.log("Login with Microsoft redirect")
                 Login with Microsoft redirect
               </BaseButton>
