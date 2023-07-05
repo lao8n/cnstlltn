8bd8cfeb7a9c233f919d63f89baf816182e725ea
Nicholas Drake <hellolao8n@gmail.com>
2023-07-06 00:38:59 +0100

    remove all /.auth/me stuff from front end instead try to do it on teh backend

--- src/web/src/pages/loginRedirect.tsx
+++ src/web/src/pages/loginRedirect.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useContext } from "react";
 import UserContext from "../components/userContext";
 import { useNavigate } from 'react-router-dom';
-import { trackEvent } from "../services/telemetryService";
-import { ActionTypes } from "../actions/common";
 import { withApplicationInsights } from "../components/telemetry";
 
 export const LoginRedirect = () => {
@@ -10,54 +8,15 @@ export const LoginRedirect = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchAuthInfo = async () => {
-            const clientPrincipal = await getAuthInfo();
-            console.log(clientPrincipal);
-            if (clientPrincipal && clientPrincipal.userId) {
-                setUser(prevUser => ({
-                    ...prevUser,
-                    userId: clientPrincipal.userId, 
-                    isAuthenticated: true,
-                }));
-                trackEvent(ActionTypes.LOGIN_REDIRECT_SET_USER.toString());
-                navigate('/constellation');
-            } else {
-                console.error('Error during authentication:');
-                setUser(prevUser => ({
-                    ...prevUser,
-                    userId: prevUser?.userId ?? '',
-                    isAuthenticated: true,
-                }));
-                navigate('/login');
-            }
-        };
-        fetchAuthInfo();
+        setUser(prevUser => ({
+            ...prevUser,
+            userId: prevUser?.userId ?? '',
+            isAuthenticated: true,
+        }));
+        navigate('/constellation');
     }, [setUser, navigate]);
     
     return null;
 };
-
-async function getAuthInfo() {  
-    const headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-    };
-    const requestOptions = {
-        method: 'GET',
-        headers: headers,
-        credentials: 'same-origin' as RequestCredentials
-    };
-    // call the endpoint  
-    const response = await fetch('/.auth/me', requestOptions);  
-    // convert to JSON  
-    const json = await response.json();  
-    // ensure clientPrincipal  exist  
-    if(json.clientPrincipal) {  
-        return json.clientPrincipal;  
-    } else {  
-        // return null if anonymous  
-        return null;  
-    }  
-}  
 
 export default withApplicationInsights(LoginRedirect, 'LoginRedirect');
