4d8c942f4545d791e96288324ff5773b43374c0e
Nicholas Drake <hellolao8n@gmail.com>
2024-01-19 15:06:04 +0000

    try adding canvas ref to display not sure if need to update css at all

--- src/web/src/components/constellationPane.tsx
+++ src/web/src/components/constellationPane.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@fluentui/react';
-import React, { FC, ReactElement, useContext, useEffect, useMemo } from "react";
+import React, { FC, ReactElement, useContext, useEffect, useMemo, useRef } from "react";
 import { clusterButtonStyles, stackItemPadding } from '../ux/styles';
 import { UserFramework } from "../models/userState";
 import { AppContext } from "../models/applicationState";
@@ -8,6 +8,7 @@ import { bindActionCreators } from "../actions/actionCreators";
 import * as userActions from '../actions/userActions';
 import { UserActions } from '../actions/userActions';
 import { ActionTypes } from '../actions/common';
+import { CanvasSpace, Circle, Pt } from "pts";
 
 interface ConstellationPaneProps {
     constellation?: UserFramework[];
@@ -15,10 +16,11 @@ interface ConstellationPaneProps {
 
 const ConstellationPane: FC<ConstellationPaneProps> = (props: ConstellationPaneProps): ReactElement => {
     const appContext = useContext<AppContext>(UserAppContext)
-    const actions = useMemo(() => ({      
+    const actions = useMemo(() => ({
         constellation: bindActionCreators(userActions, appContext.dispatch) as unknown as UserActions
     }), [appContext.dispatch]);
-    const clusterbyquery = "political, economic, sociological, technological, legal, environmental"
+    const clusterbyquery = "political, economic, sociological, technological, legal, environmental, psychological"
+    const canvasRef = useRef<HTMLCanvasElement>(null); // Create a ref for the canvas
 
     useEffect(() => {
         const getConstellation = async () => {
@@ -31,13 +33,48 @@ const ConstellationPane: FC<ConstellationPaneProps> = (props: ConstellationPaneP
             return constellation
         };
         getConstellation();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [actions.constellation, appContext.dispatch]);
 
     const clusterBy = async () => {
         const clustered = await actions.constellation.cluster(appContext.state.userState.userId, clusterbyquery)
         console.log("clustered " + clustered)
     };
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const space = new CanvasSpace(canvasRef.current).setup({ bgcolor: "#123" });
+            const form = space.getForm();
+            let pts : Pt[] = [];
+
+            space.add({
+                start: (bound) => {
+                    if (props.constellation) {
+                        pts = props.constellation.map((framework) => {
+                            return new Pt(framework.clusterby[clusterbyquery].coordinates[0], framework.clusterby[clusterbyquery].coordinates[1])
+                        })
+                    }
+                },
+                animate: (time, ftime) => {
+                    const r = Math.abs(space.pointer.x - space.center.x) / space.center.x * 150 + 70;
+                    const range = Circle.fromCenter(space.pointer, r);
+                    const colors = ["#ff2d5d", "#42dc8e", "#2e43eb", "#ffe359"]; // Define all colors
+                    
+                    for (let i = 0, len = pts?.length; i < len; i++) {
+                        if (Circle.withinBound(range, pts[i])) {
+                            const dist = (r - pts[i].$subtract(space.pointer).magnitude()) / r;
+                            const p = pts[i].$subtract(space.pointer).scale(1 + dist).add(space.pointer);
+                            form.fillOnly(colors[i % colors.length]).point(p, dist * 25, "circle");
+                        } else {
+                            form.fillOnly("#fff").point(pts[i], 0.5);
+                        }
+                    }
+                }
+            });
+
+            space.bindMouse().bindTouch().play();
+        }
+    }, [props.constellation]);
 
     return (
         <Stack>
@@ -54,11 +91,7 @@ const ConstellationPane: FC<ConstellationPaneProps> = (props: ConstellationPaneP
                 </Stack.Item>
             </Stack>
             <Stack.Item>
-                {props.constellation?.map((constellation, index) => (
-                    <div key={index}>
-                        {constellation.title}: {constellation.content} - {constellation.clusterby[clusterbyquery].cluster} - {constellation.clusterby[clusterbyquery].coordinates}
-                    </div>
-                ))}
+                <canvas ref={canvasRef} id="pt" />
             </Stack.Item>
         </Stack>
     )
