6e9a69c3aab25765f246497614fd280b11db86ac
Nicholas Drake <hellolao8n@gmail.com>
2024-07-21 16:55:51 +0100

    try using viewport height to set overall height

--- src/web/src/ux/styles.ts
+++ src/web/src/ux/styles.ts
@@ -44,9 +44,9 @@ export const canvasStackStyle: IStackStyles = {
     root: {
         // minHeight: 900,
         // maxHeight: 1200,
-        overflow: 'hidden',
-        width: '100%',
-        height: '100%',
+        overflow: 'auto',
+        width: '100vw',
+        height: '100vh',
     }
 }
 
