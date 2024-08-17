1b8bf18491eafc56e572308ed6d8194b93135b7e
Nicholas Drake <hellolao8n@gmail.com>
2024-08-17 14:47:21 +0100

    resize doesn't change dimensions that were setup initially but at least when you select on welcome screen it does something

--- src/web/src/ux/components.ts
+++ src/web/src/ux/components.ts
@@ -15,8 +15,10 @@ export const canvasStackStyle: IStackStyles = {
 }
 
 export const canvasStyle = {
+    display: 'flex',
     width: '100%',
     height: '100%',
+    overflow: 'hidden',
 };
 
 // Define styles for the button
