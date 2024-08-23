770bb07b459acf2774872159cd75deae2bac0a27
Nicholas Drake <hellolao8n@gmail.com>
2024-08-23 17:08:23 +0100

    clean up bad import

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,12 +1,15 @@
+// react imports
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import RoutesLayout from './pages/routes';
-import './App.css';
-import { CnstlltnTheme } from './ux/theme';
-import { UserAppProvider } from './panes/user';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { ThemeProvider } from '@fluentui/react';
+// pages & panes imports
+import RoutesLayout from './pages/routes';
+import { UserAppProvider } from './panes/user';
 import Telemetry from './telemetry/telemetry';
+// ux imports
+import './App.css';
+import { CnstlltnTheme } from './ux/shared/theme';
 
 export const App: FC = () => {
   initializeIcons();
