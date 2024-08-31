1d63dcbe29af902cb3e7ea488f8d64af49642b28
Nicholas Drake <hellolao8n@gmail.com>
2024-08-31 01:52:54 +0100

    add debugging for why title not matching

--- src/web/src/panes/constellationPane.tsx
+++ src/web/src/panes/constellationPane.tsx
@@ -235,6 +235,7 @@ const ConstellationPane: FC = (): ReactElement => {
                             } else {
                                 pt.selected = !pt.selected;
                                 actions.note.setSelectedContent(pt.selected ? pt.userFramework : null);
+                                console.log("selected content: ", appContext.state.userState.selectedContent?.title)
                             }
                         }
                     });
